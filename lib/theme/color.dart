aac6f68f7d5d427979ed6ac46efe8829a714ebd2
HydraCoder <ngodat.it213@gmail.com>
2023-04-18 00:03:20 +0700

    update component image, color and pubspec.yaml

---
+++ lib/theme/color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color titleActive = Color(0xFF000000);
+  static const Color body = Color(0xFF333333);
+  static const Color label = Color(0xFF555555);
+  static const Color placeholder = Color(0xFF888888);
+  static const Color line = Color(0xFF14142B);
+  static const Color inputBackground = Color(0xFFF9F9F9);
+  static const Color background = Color(0xFFF8F0E7);
+  static const Color offWhite = Color(0xFFFCFCFC);
+  static const Color primary = Color(0xFFA8715A);
+  static const Color secondary = Color(0xFFDD8560);
+}
